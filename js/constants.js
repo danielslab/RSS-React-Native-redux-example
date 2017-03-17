d4b2d3f9f1fcf8c709fc6b015c3aff5151024a5c
swoopyy <swoopyyy@gmail.com>
2017-03-17 23:42:29 +0300

    move icons to constants

--- js/constants.js
+++ js/constants.js
@@ -1,8 +1,40 @@
-//styles
+import {Platform} from 'react-native';
+
+// styles
 export const MAIN_COLOR = '#2196F3';
-const IOS_BACKGROUND_COLOR = '#efeff4';
+export const IOS_BACKGROUND_COLOR = '#efeff4';
+export const STAR_COLOR = '#FFD600';
+
+// icons
+const getIcons = () => {
+    if (Platform.OS === 'ios') {
+        return {
+            paperPlane: 'ios-paper-plane',
+            priceTags: 'ios-pricetags',
+            settings: 'ios-settings',
+            menu: 'ios-menu',
+            refresh: 'ios-refresh',
+            star: 'ios-star',
+        }
+    } else {
+        return {
+            paperPlane: 'md-paper-plane',
+            priceTags: 'md-pricetags',
+            settings: 'md-settings',
+            menu: 'md-menu',
+            refresh: 'md-refresh',
+            star: 'md-star'
+        }
+    }
+};
+export const icons = getIcons();
 
 //actions
 export const ON_ALL_SELECTED = 'ON_ALL_SELECTED';
 export const ON_TAGS_SELECTED = 'ON_TAGS_SELECTED';
 export const ON_MANAGE_CHANNELS_SELECTED = 'ON_MANAGE_CHANNELS_SELECTED';
+export const GET_FEEDS = 'GET_FEEDS';
+export const ON_FEED_PRESS = 'ON_FEED_PRESS';
+export const ON_TAP_STAR = 'ON_TAP_STAR';
+export const SELECT_FEED_TAB = 'SELECT_FEED_TAB';
+export const REFRESH_FEEDS = 'REFRESH_FEEDS';
