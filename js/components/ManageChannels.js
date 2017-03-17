e5e4949075b7f7eeebcdbf90b3d650b556f4d131
swoopyy <swoopyyy@gmail.com>
2017-03-17 23:42:00 +0300

    add Tags and ManageChannel mock components

--- js/components/ManageChannels.js
+++ js/components/ManageChannels.js
@@ -1,3 +1,22 @@
 /**
  * Created by denissamohvalov on 17.03.17.
  */
+import React, {Component, PropTypes} from 'react';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {
+    StyleSheet,
+    Platform,
+    View,
+    Text,
+    TouchableOpacity,
+    Navigator
+} from 'react-native';
+import {MAIN_COLOR} from '../constants';
+
+export default class ManageChannels extends Component {
+    render() {
+        return <View/>;
+    }
+}
