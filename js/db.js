d3cde5fed007c26f929294fafd0356d7e44fa38b
swoopyy <swoopyyy@gmail.com>
2017-03-17 23:42:39 +0300

    add db schema

--- js/db.js
+++ js/db.js
@@ -1,1 +1,15 @@
 let Realm = require('realm');
+
+const FeedSchema = {
+    name: 'Feed',
+    properties: {
+        id: 'string',
+        is_bookmarked: 'bool',
+        title: 'string',
+        subtitle: 'string',
+        faviconUrl: 'string'
+    }
+};
+
+let realm = new Realm({schema: [FeedSchema], schemaVersion: 1});
+export default realm;
