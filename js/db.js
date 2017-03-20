ff2a4b8df07ac065f3baf0a6387ba2e95aafb8b6
swoopyy <swoopyyy@gmail.com>
2017-03-20 22:36:01 +0300

    update DB Schema

--- js/db.js
+++ js/db.js
@@ -1,9 +1,10 @@
-let Realm = require('realm');
+import Realm from 'realm';
 
 const FeedSchema = {
     name: 'Feed',
     properties: {
         id: 'string',
+        channel: 'Channel',
         is_bookmarked: 'bool',
         title: 'string',
         subtitle: 'string',
@@ -11,5 +12,37 @@ const FeedSchema = {
     }
 };
 
-let realm = new Realm({schema: [FeedSchema], schemaVersion: 1});
+const ChannelSchema = {
+    name: 'Channel',
+    properties: {
+        id: 'string',
+        feeds: {
+            type: 'list',
+            objectType: 'Feed',
+        },
+        tagsMask: {
+            type: 'list',
+            objectType: 'TagMask'
+        },
+        url: 'string',
+        name: 'string',
+    }
+};
+
+const TagMaskSchema = {
+    name: 'TagMask',
+    properties: {
+        name: 'string',
+        isChecked: 'bool',
+    }
+};
+
+const TagSchema = {
+    name: 'Tag',
+    properties: {
+        name: 'string',
+    }
+};
+
+let realm = new Realm({schema: [FeedSchema, ChannelSchema, TagMaskSchema, TagSchema], schemaVersion: 2});
 export default realm;
